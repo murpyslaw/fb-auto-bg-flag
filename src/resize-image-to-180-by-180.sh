f76f7c29e5367295250aa23ab82237af5595e98f
murphyslaw <rivanov2@protonmail.com>
2017-07-05 01:32:18 -0500

    :nut_and_bolt: auto-orientation support

--- src/resize-image-to-180-by-180.sh
+++ src/resize-image-to-180-by-180.sh
@@ -2,5 +2,5 @@
 args=("$@")
 first=${args[0]}
 res_path=${first}-180x180.png
-convert ${args[0]} -resize 180x180\! ${res_path}
+convert ${first} -auto-orient -resize 180x180\! ${res_path}
 mv ${res_path} ../tmp
